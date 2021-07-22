30114126a889e4ee603c63177f68515b5efeae73
Phaneendrakumar9 <phaneendrakumar116@gmail.com>
2021-07-22 15:45:02 +0530

    Added Schema and models

---
+++ database/book.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose");
+
+//Creating a Book Schema
+const BookSchema=mongoose.Schema({
+    ISBN:String,
+    title:String,
+    pubDate:String,
+    language:String,
+    numPage:Number,
+    author:[Number],
+    publication:Number,
+    category:[String]
+});
+
+//Create a book model
+const BookModel = mongoose.model(BookSchema);
+
+module.exports=BookModel;
